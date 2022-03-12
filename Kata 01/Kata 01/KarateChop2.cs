eeb57708f657df0d5114f3dbf7abf84466223b95
David Ullrich <david.ullrich@ullrich-software.at>
2022-03-12 23:26:28 +0100

    Recursive solution added

---
+++ Kata 01/Kata 01/KarateChop2.cs
@@ -0,0 +1,25 @@
+﻿namespace Kata_01;
+
+/// <summary>
+/// Simple recursive solution
+/// </summary>
+public class KarateChop2
+{
+    public int Chop(int[] arr, int search) => 
+        Chop(arr: arr, search: search, start: 0, end: arr.Length - 1);
+
+    private int Chop(IReadOnlyList<int> arr, int search, int start, int end)
+    {
+        if (end < start)
+            return -1;
+        
+        var index = start + (end - start) / 2;
+
+        if (arr[index] == search)
+            return index;
+
+        return arr[index] > search 
+            ? Chop(arr: arr, search: search, start: start, end: index - 1) 
+            : Chop(arr: arr, search: search, start: index + 1, end: end);
+    }
+}
---
+++ Kata 01/Kata 01/Tests/KarateChop2Tests.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace Kata_01.Tests;
+
+public class KarateChop2Tests
+{
+    private readonly ITestOutputHelper _output;
+
+    public KarateChop2Tests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    [Theory]
+    [ClassData(typeof(TestData))]
+    public void Test_chop(int[] arr, int search, int expected)
+    {
+        // Arrange
+        var sut = new KarateChop2();
+
+        // Act
+        var actual = sut.Chop(arr: arr, search: search);
+
+        _output.WriteLine($"arr: {string.Join(",", arr)}, " +
+                          $"search: {search}, " +
+                          $"expected: {expected}, " +
+                          $"actual: {actual}");
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
